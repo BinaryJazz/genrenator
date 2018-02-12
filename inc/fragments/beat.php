42a875cd387f150b47f77927c8a6871ec7047eea
Chris Reynolds <chris@jazzsequence.com>
2018-02-12 12:30:31 -0700

    add more beats

--- inc/fragments/beat.php
+++ inc/fragments/beat.php
@@ -6,13 +6,28 @@ namespace BinaryJazz\Genrenator\Fragments;
 class beat extends fragments {
 
 	private function beats() {
-		$adjective = new adjective();
+		$adjective  = new adjective();
 		$instrument = new instrument();
+		$region     = new region();
 		return [
 			// Translators: %s is a random adjective.
 			sprintf( __( '%s beat', 'genrenator' ), $adjective->get_element() ),
 			// Translators: %s is a random adjective.
 			sprintf( __( '%s bass', 'genrenator' ), $adjective->get_element() ),
+			// Translators: %s is a random adjective.
+			sprintf( __( '%s listening', 'genrenator' ), $adjective->get_element() ),
+			// Translators: %s is a random adjective.
+			sprintf( __( '%s band', 'genrenator' ), $adjective->get_element() ),
+			// Translators: %s is a random adjective.
+			sprintf( __( '%s tunes', 'genrenator' ), $adjective->get_element() ),
+			// Translators: %s is a random adjective.
+			sprintf( __( '%s era', 'genrenator' ), $adjective->get_element() ),
+			// Translators: %s is a random adjective.
+			sprintf( __( '%s dawn', 'genrenator' ), $adjective->get_element() ),
+			// Translators: %s is a random region.
+			sprintf( __( '%s sound', 'genrenator' ), $region->get_element() ),
+			// Translators: %s is a random region.
+			sprintf( __( '%sinvasion', 'genrenator' ), $region->get_element() ),
 			sprintf( '%1$s %2$s', $adjective->get_element(), $instrument->get_element() ),
 		];
 	}
