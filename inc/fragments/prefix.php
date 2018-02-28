16f99f2ec0d49cd136abed1e6be4335438d6dcdf
Chris Reynolds <chris@jazzsequence.com>
2018-02-28 11:37:10 -0700

    remove stuff that was moved into adjectives

--- inc/fragments/prefix.php
+++ inc/fragments/prefix.php
@@ -79,51 +79,7 @@ class prefix extends fragments {
 		'euro',
 	];
 
-	private function translatable_prefixes() {
-		$instrument = new instrument();
-		return [
-			__( 'stomp and ', 'genrenator' ),   // Since this is a phrase, we'll make it translatable.
-			__( 'drum and ', 'genrenator' ),    // This is also a phrase.
-			__( 'jig and ', 'genrenator' ),     // This is also a phrase.
-			__( 'rhythm and ', 'genrenator' ),  // This is also a phrase.
-			// Translators: %s is a random instrument.
-			sprintf( __( '%s-driven ', 'genrenator' ), $instrument->get_element() ),
-			// Translators: %s is a random instrument.
-			sprintf( __( '%s-heavy ', 'genrenator' ), $instrument->get_element() ),
-			// Translators: %s is a random instrument.
-			sprintf( __( '%s-based ', 'genrenator' ), $instrument->get_element() ),
-			// Translators: %s is a random instrument.
-			sprintf( __( '%s-centric ', 'genrenator' ), $instrument->get_element() ),
-		];
-	}
-
 	public function elements() {
-		return array_merge( self::PREFIXES, $this->translatable_prefixes() );
+		return self::PREFIXES;
 	}
-
-	public function texturize() {
-		return parent::texturize();
-	}
-
-	/**
-	 * Return an array of prefixes that don't have spaces.
-	 *
-	 * @since  0.2
-	 * @return array Prefixes that don't have spaces.
-	 */
-	public static function no_space_prefixes() {
-		static $kellen;
-
-		if ( count( $kellen ) ) {
-			return $kellen;
-		}
-
-		$prefixes = self::PREFIXES;
-		$kellen   = array_filter( $prefixes, function ( $prefix ) {
-			return strpos( $prefix, ' ' ) === false;
-		} );
-
-		return $kellen;
-	}
-
 }
